3084c13edfe996dddbcaf57db761bf03688065c4
NexusLee <tonexus.lee@gmail.com>
2016-10-28 14:21:49 +0800

    logger

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
-'use strict'
+'use strict';
 
 var express = require('express');
 var app = express();
+var logger = require('./lib/logger');
 
 var server = app.listen('3000', function(){
     var host = server.address().address;
     var port = server.address().port;
-    console.log('Server start at http://%s:%s', '0.0.0.0', 3000);
-})
+    logger.info('Server start at http://%s:%s', '0.0.0.0', 3000);
+});
